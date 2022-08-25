74941ed52912476c0cb6d221159a190949bfb369
TheLocagus <locagus@o2.pl>
2022-08-25 15:11:11 +0200

    Changed few styles. Changed apiUrl.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,1 @@
-export const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3002';
-export const frontUrl = process.env.REACT_APP ?? 'http://localhost:3000';
+export const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3002/api';
